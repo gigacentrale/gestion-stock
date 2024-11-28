8411963736e453bf359f84ca633e88f470df2b8e
gigacentrale <gigacentrale@gmail.com>
2024-11-29 00:36:14 +0100

    Create CommandeAchat.js

---
+++ src/pages/CommandeAchat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CommandeAchat = () => {
+  return (
+    <div>
+      <h1>Commande Achat</h1>
+      {/* Ajouter le formulaire de commande achat ici */}
+    </div>
+  );
+};
+
+export default CommandeAchat;
